eb9619889708f57945469047d8e18aaefeb9251a
Yentl Hendrickx <r0842908@student.thomasmore.be>
2022-02-19 23:47:58 +0100

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,26 @@
-// window.addEventListener('DOMContentLoaded', event => {
+$(document).ready(function () {
 
-//     // Navbar shrink function
-//     var navbarShrink = function () {
-//         const navbarCollapsible = document.body.querySelector('#navbar');
-//         if (!navbarCollapsible) {
-//             return;
-//         }
-//         if (window.scrollY === 0) {
-//             navbarCollapsible.classList.remove('navbar-shrink')
-//         } else {
-//             navbarCollapsible.classList.add('navbar-shrink')
-//         }
+    $('section:not(.main)').hide();
+    $('.shared').hide();
 
-//     };
+    $('.more-info').click(function(event){
+        event.preventDefault();
+        console.log("TEST");
+        let id = $(this).attr('id');
+        console.log(id);
 
-//     // Shrink the navbar 
-//     navbarShrink();
-
-//     // Shrink the navbar when page is scrolled
-//     document.addEventListener('scroll', navbarShrink);
-
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const mainNav = document.body.querySelector('#navbar');
-//     if (mainNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#mainNav',
-//             offset: 72,
-//         });
-//     };
-
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
-
-// });
+        if (id != undefined) {
+            $('.main').hide();
+            $('.shared').show();
+            $('section#' + id).show();
+            window.scrollTo(0,0);
+        }
+    });
+    
+    $('#return').click(function(event){
+        event.preventDefault();
+        $('.main').show();
+        $('section:not(.main)').hide();
+        window.scrollTo(0,0);
+    });
+});
