b354afa9f91f3a5acf55ab9c93349b78c05b46bd
Yentl Hendrickx <r0842908@student.thomasmore.be>
2022-05-03 23:03:07 +0200

    reformatting page, multiple html files, better navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $('section:not(.main)').hide();
-    $('.shared').hide();
+//     $('section:not(.main)').hide();
+//     $('.shared').hide();
 
-    $('.more-info').click(function(event){
-        event.preventDefault();
-        let id = $(this).attr('id');
-        console.log(id);
+//     $('.more-info').click(function(event){
+//         event.preventDefault();
+//         let id = $(this).attr('id');
+//         console.log(id);
 
-        if (id != undefined) {
-            $('.navbar-collapse').collapse('hide');
-            $('.main').hide();
-            $('.shared').show();
-            $('section#' + id).show();
-            window.scrollTo(0,0);
-        }
-    });
+//         if (id != undefined) {
+//             $('.navbar-collapse').collapse('hide');
+//             $('.main').hide();
+//             $('.shared').show();
+//             $('section#' + id).show();
+//             window.scrollTo(0,0);
+//         }
+//     });
     
-    $('#return').click(function(event){
-        event.preventDefault();
-        $('.main').show();
-        $('.shared').hide();
-        $('section:not(.main)').hide();
-        $('.navbar-collapse').collapse('hide');
-    });
-});
+//     $('#return').click(function(event){
+//         event.preventDefault();
+//         $('.main').show();
+//         $('.shared').hide();
+//         $('section:not(.main)').hide();
+//         $('.navbar-collapse').collapse('hide');
+//     });
+// });
